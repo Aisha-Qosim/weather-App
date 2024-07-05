6f2305049a311c35c4bbda7de26508dda1016341
Qosim Aisha <qosimaishaoyindamola@gmail.com>
2024-07-04 21:24:20 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let search_btn = document.getElementById("search_btn");
+function currentTime(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+document.getElementById("displayCurrentTime").innerHTML = currentTime(new Date());
+
+  function search(){
+   
+    let inputState = document.getElementById("searchState").value
+    let displayCityName = document.getElementById("displayCityName")
+    let key =
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputState}&appid=84a5541e536549f59b50ed907cab345b&units=metric`;
+      fetch(key)
+      .then((res) => res.json())
+      .then((data)=>{
+        console.log(data)
+        // console.log(data.weather[0])
+        description.innerHTML = data.weather[0].main 
+
+        description.innerHTML = data.weather[0].description 
+
+        displayCityTemparature.innerHTML= Math.round(data.main.temp) +`&deg;C`
+        displayCityName.innerHTML = `${data.name},${data.sys.country}`
+        
+      })
+      .catch((err) =>displayCityName.innerHTML= `<p>City not found </p>`);
+     
+    
+   
+    }
+
+search_btn.addEventListener("click",search)
